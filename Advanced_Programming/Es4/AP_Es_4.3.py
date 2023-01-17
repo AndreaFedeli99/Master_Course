e1044717f210c5176c2787516123b767d3bca19e
Andrea Fedeli <andreafedeli@live.com>
2023-01-17 22:43:40 +0100

    Added solution to exercise 4.3

--- Advanced_Programming/Es4/AP_Es_4.3.py
+++ Advanced_Programming/Es4/AP_Es_4.3.py
@@ -9,3 +9,45 @@
 # 1. at each step will return another iterator containing all the elements in the corresponding stage sorted as in the triangle
 # 2. the iterator can go forward and backward
 # 3. (advanced) it is possible to define the working algebra for the triangle, e.g., it could be on Z7 or on the alphabet
+
+class PascalTriangle:
+
+    def __init__(self, stage):
+        self._triangle = [[1], [1, 1]]
+        self._stage = stage
+
+    def __iter__(self):
+        self.n = 0
+        self.__calculate_triangle()
+        return self
+    
+    def __next__(self):
+        if self.n < self._stage:
+            res = self._triangle[self.n]
+            self.n += 1
+            return res
+        raise StopIteration
+
+    def prev(self):
+        if self.n > 0:
+            res = self._triangle[self.n - 1]
+            self.n -= 1
+            return res
+        raise StopIteration
+
+    def __calculate_triangle(self):
+        def inner(res):
+            if len(self._triangle) == self._stage : return res
+            res.append([1] + [res[-1][i] + res[-1][i + 1] for i in range(0, len(res[-1]) - 1)] + [1])
+            return inner(res)
+        return inner(self._triangle)
+
+t = PascalTriangle(5)
+
+print("Pascal triangle:")
+for i in t:
+    print(*t, sep='\n')
+
+print("Reversed Pascal triangle:")
+for i in range(t._stage):
+    print(t.prev())
