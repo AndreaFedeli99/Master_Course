a50df4f8647417822d6cfea751751b8e823f9979
Andrea Fedeli <andreafedeli@live.com>
2023-02-04 16:28:41 +0100

    Added solution to exercise 7.2

--- Advanced_Programming/Es7/AP_Es_7.2.py
+++ Advanced_Programming/Es7/AP_Es_7.2.py
@@ -12,3 +12,28 @@
 # Write the following Python functions:
 # 1. goldbach(n) that returns a Goldbach partition for n
 # 2. goldbach_list(n,m) that returns a dictionary indexed on the even numbers in the range (n,m) and whose values are their Goldbach partition
+
+from itertools import combinations_with_replacement
+
+def goldbach(n):
+    if n <= 2 or n % 2 != 0: return
+    def generate_primes(n):
+        sieve = [True for i in range(2, n+1)]
+        for i in range(int(n**0.5) + 1):
+            if sieve[i]:
+                x = 2
+                j = (i + 2) * x
+                while j < n + 1:
+                    sieve[j - 2] = False
+                    x += 1
+                    j = (i + 2) * x
+        return [i for i in range(2, len(sieve)) if sieve[i - 2]]
+    return [i for i in combinations_with_replacement(generate_primes(n), 2) if sum(i) == n]
+
+def goldbach_list(n, m):
+    return {k: goldbach(k) for k in range(n, m + 1) if k % 2 == 0}
+
+if __name__ == '__main__':
+    print(goldbach(3))
+    print(goldbach(20))
+    print(goldbach_list(4, 20))
