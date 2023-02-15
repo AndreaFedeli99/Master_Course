7e32379e9c72c6374e14fcce9346335057a5fdd8
Andrea Fedeli <andreafedeli@live.com>
2023-02-15 23:28:13 +0100

    Optimazed code

--- Advanced_Programming/Exams/2022-02-15/Es1/anagrams.py
+++ Advanced_Programming/Exams/2022-02-15/Es1/anagrams.py
@@ -1,15 +1,17 @@
 from itertools import groupby
 
-FILE_NAME = "wordlist-text.txt"
-
-words = [w.strip().lower() for w in open(FILE_NAME).readlines()]
+FILE_NAME = "Università\Advanced_Programming\Exams\\2022-02-15\Es1\wordlist-text.txt"
 
 def anagram_key(word):
     return ''.join(sorted(word))
 
+words = [w.strip().lower() for w in open(FILE_NAME).readlines()]
+words_dict = {k: list(v) for k,v in groupby(sorted(words, key=lambda x: anagram_key(x)), key=lambda x: anagram_key(x))}
+
+
 def anagram(word):
-    return ', '.join([w for w in words if anagram_key(word) == anagram_key(w) and w != word])
+    key = ''.join(sorted(word))
+    return ', '.join([v for v in words_dict[key] if v != word])
     
 def anagrams():
-    groups = {k: list(v) for k,v in groupby(sorted(words, key=lambda x: anagram_key(x)), key=lambda x: anagram_key(x))}
-    return '\n'.join([f"{k} :- {', '.join(v)}" for k,v in sorted({v[0]: v[1:] for k,v in groups.items()}.items(), key = lambda x: x[0])])
+    return '\n'.join([f"{k} :- {', '.join(v)}" for k,v in sorted({v[0]: v[1:] for k,v in words_dict.items()}.items(), key = lambda x: x[0])])
